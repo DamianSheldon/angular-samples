83012402470157637669e3e6d073478eec5efb5f
Meiliang Dong <dongmeilianghy@sina.com>
2018-07-24 10:20:53 +0800

    Added routing.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,9 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
