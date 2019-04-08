0e4d881bc489ef233613526c1faef26a106b1e02
Meiliang Dong <dongmeilianghy@sina.com>
2019-04-08 16:46:18 +0800

    Add heroes functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AppRoutingModule } from './/app-routing.module';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
