c6adf5bc39c15c73abac2456be2daf0b0f7c06c2
Meiliang Dong <dongmeilianghy@sina.com>
2019-04-09 17:09:44 +0800

    Teach AuthGuard to authenticate

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    console.log('AuthGuard#canActivate called');
+    state: RouterStateSnapshot): boolean {
     return true;
+  }
+
+  checkLogin(url: string): boolean {
+    if (this.authService.isLoggedIn) {
+      return ture;
+    }
+
+    // Store the attempted URL for redirecting
+    this.authService.redirectUrl = url;
+
+    // Navigate to the login page with extras
+    this.router.navigate(['/login']);
+
+    return false;
   }
 }
